e524c248dabdef2885c5b2951a76e7aefa960260
destroylord <masapin68@gmail.com>
2020-12-23 17:33:15 +0700

    tampil semua data

---
+++ resources/views/employee.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <section style="padding-top:60px; margin-bottom:50px">
+        <div class="container">
+            <h1>Hello world</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    {!! $dataTable->table() !!}
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {!! $dataTable->scripts() !!}
+  </body>
+</html>
